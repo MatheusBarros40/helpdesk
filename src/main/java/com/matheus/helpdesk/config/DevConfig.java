6cebb26825c86ee8f525695c9841b4093ee4625f
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-18 19:47:35 -0300

    perfil de desenvolvimento criado

---
+++ src/main/java/com/matheus/helpdesk/config/DevConfig.java
@@ -0,0 +1,25 @@
+package com.matheus.helpdesk.config;
+
+import com.matheus.helpdesk.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+    @Bean
+    public boolean instanciaDB(){
+        if(value.equals("create")){
+            this.dbService.instanciaDB();
+        }
+        return false;
+    }
+}
