7313e90bafb932ecd1f72ad6db72a35ea8bbc287
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-18 12:46:30 -0300

    Criação dos Enums

---
+++ src/main/java/com/matheus/helpdesk/domain/enums/Status.java
@@ -0,0 +1,35 @@
+package com.matheus.helpdesk.domain.enums;
+
+public enum Status {
+
+    ABERTO(0, "ABERTO"),ANDAMENTO(1, "ANDAMENTO"),ENCERRADO(2, "ENCERRADO");
+
+    private Integer codigo;
+    private String descricao;
+
+    Status(Integer codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Status toEnum(Integer cod) {
+        if(cod == null){
+            return null;
+        }
+        for(Status x : Status.values()) {
+            if(cod.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Status inválido");
+    }
+}
