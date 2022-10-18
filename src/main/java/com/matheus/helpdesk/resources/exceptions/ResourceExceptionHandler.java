a0a0695e69cf0c6b015547375f125c7e22c789f2
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-18 20:57:11 -0300

    Excecao para objeto nao encontrado

---
+++ src/main/java/com/matheus/helpdesk/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.matheus.helpdesk.resources.exceptions;
+
+import com.matheus.helpdesk.service.exceptions.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException ex, HttpServletRequest request){
+        StandardError error = new StandardError(
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                "Object not found.",
+                ex.getMessage(),
+                request.getRequestURI()
+                );  return  ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+}
--- src/main/java/com/matheus/helpdesk/service/TecnicoService.java
+++ src/main/java/com/matheus/helpdesk/service/TecnicoService.java
@@ -2,6 +2,7 @@ package com.matheus.helpdesk.service;
 
 import com.matheus.helpdesk.domain.Tecnico;
 import com.matheus.helpdesk.repositories.TecnicoRepository;
+import com.matheus.helpdesk.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,6 @@ public class TecnicoService {
 
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id :" + id));
     }
 }
