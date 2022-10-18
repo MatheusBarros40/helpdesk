90f119145ec2684fc3f0515219f707a173767457
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-18 11:42:56 -0300

    Initial commit

---
+++ src/test/java/com/matheus/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
