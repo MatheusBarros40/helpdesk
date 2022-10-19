478db41202767f12b81f2c9627666110894ca7d2
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-19 20:48:07 -0300

    crud Cliente

--- src/main/java/com/matheus/helpdesk/domain/Cliente.java
+++ src/main/java/com/matheus/helpdesk/domain/Cliente.java
@@ -1,12 +1,16 @@
 package com.matheus.helpdesk.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import com.matheus.helpdesk.domain.dto.ClienteDTO;
 import com.matheus.helpdesk.domain.enums.Perfil;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Entity
 public class Cliente extends Pessoa{
     private static final long serialVersionUID = 1L;
@@ -22,6 +26,19 @@ public class Cliente extends Pessoa{
         super(id, nome, cpf, email, senha);
         addPerfil(Perfil.CLIENTE);
     }
+
+    public Cliente(ClienteDTO obj) {
+        super();
+        this.id = obj.getId();
+        this.nome = obj.getNome();
+        this.cpf = obj.getCpf();
+        this.email = obj.getEmail();
+        this.senha = obj.getSenha();
+        this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+        this.dataCriacao = obj.getDataCriacao();
+    }
+
+
 
     public List<Chamado> getChamados() {
         return chamados;
---
+++ src/main/java/com/matheus/helpdesk/service/ClienteService.java
@@ -0,0 +1,70 @@
+package com.matheus.helpdesk.service;
+
+import com.matheus.helpdesk.domain.Pessoa;
+import com.matheus.helpdesk.domain.Cliente;
+import com.matheus.helpdesk.domain.dto.ClienteDTO;
+import com.matheus.helpdesk.repositories.PessoaRepository;
+import com.matheus.helpdesk.repositories.ClienteRepository;
+import com.matheus.helpdesk.service.exceptions.DataIntegrityViolationException;
+import com.matheus.helpdesk.service.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Cliente findById(Integer id){
+        Optional<Cliente> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id :" + id));
+    }
+
+    public List<Cliente> findAll() {
+        return repository.findAll();
+    }
+
+    public Cliente create(ClienteDTO objDTO) {
+        objDTO.setId(null);
+        validaPorCpfEEmail(objDTO);
+        Cliente newObj = new Cliente(objDTO);
+        return repository.save(newObj);
+    }
+
+    public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
+        objDTO.setId(id);
+        Cliente oldObj = findById(id);
+        validaPorCpfEEmail(objDTO);
+        oldObj = new Cliente(objDTO);
+        return repository.save(oldObj);
+    }
+    public void delete(Integer id) {
+        Cliente obj = findById(id);
+        if (obj.getChamados().size() > 0){
+            throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!!");
+        }
+        repository.deleteById(id);
+    }
+
+    private void validaPorCpfEEmail(ClienteDTO objDTO) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+        }
+
+        obj = pessoaRepository.findByEmail(objDTO.getEmail());
+        if(obj.isPresent() && obj.get().getId() != objDTO.getId()){
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema");
+        }
+    }
+
+
+}
