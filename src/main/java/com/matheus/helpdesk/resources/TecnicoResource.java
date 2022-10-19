45c63da8749172c723f2b8dde18f3b5466e0cc36
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-18 21:08:28 -0300

    Endpoint findall para tecnico

--- src/main/java/com/matheus/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/matheus/helpdesk/resources/TecnicoResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
@@ -21,5 +24,11 @@ public class TecnicoResource {
     public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
         Tecnico obj = this.service.findById(id);
         return ResponseEntity.ok().body(new TecnicoDTO(obj));
+    }
+    @GetMapping
+    public ResponseEntity<List<TecnicoDTO>> findAll(){
+        List<Tecnico> list = service.findAll();
+        List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/matheus/helpdesk/service/TecnicoService.java
+++ src/main/java/com/matheus/helpdesk/service/TecnicoService.java
@@ -6,6 +6,7 @@ import com.matheus.helpdesk.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id :" + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return repository.findAll();
     }
 }
