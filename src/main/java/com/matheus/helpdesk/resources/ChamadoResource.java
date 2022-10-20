fd77fc6b620689795907f5b0fb6afa9165ed6764
MatheusBarros40 <91424199+MatheusBarros40@users.noreply.github.com>
2022-10-19 21:15:50 -0300

    Find all chamados

--- src/main/java/com/matheus/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/matheus/helpdesk/resources/ChamadoResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/chamados")
 public class ChamadoResource {
@@ -21,5 +24,12 @@ public class ChamadoResource {
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
         Chamado obj = service.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll(){
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/matheus/helpdesk/service/ChamadoService.java
+++ src/main/java/com/matheus/helpdesk/service/ChamadoService.java
@@ -7,6 +7,7 @@ import com.matheus.helpdesk.service.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,9 @@ public class ChamadoService {
     public Chamado findById(Integer id){
         Optional<Chamado> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID:" + id));
+    }
+
+    public List<Chamado> findAll() {
+        return repository.findAll();
     }
 }
